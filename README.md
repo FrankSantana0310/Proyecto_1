70bbdcd5db1a35a65dc0fce8db06c80b4fc3b501
Frank Santana <frigsamo@gmail.com>
2020-01-28 13:08:54 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Proyecto_1
-Basic example of react using react  states  and forms
---
+++ precache-manifest.28344db6e01d637f824e1ec3cde640c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f6147d24d6344a44d176b9e8ba3eed",
+    "url": "/Proyecto_1/index.html"
+  },
+  {
+    "revision": "464fe2a57719dfad648b",
+    "url": "/Proyecto_1/static/css/main.c6b7d949.chunk.css"
+  },
+  {
+    "revision": "dd710256ffbe6888d701",
+    "url": "/Proyecto_1/static/js/2.a7493712.chunk.js"
+  },
+  {
+    "revision": "464fe2a57719dfad648b",
+    "url": "/Proyecto_1/static/js/main.2e3c6c6d.chunk.js"
+  },
+  {
+    "revision": "461595cab52b8623ff83",
+    "url": "/Proyecto_1/static/js/runtime-main.f937889a.js"
+  }
+]);
